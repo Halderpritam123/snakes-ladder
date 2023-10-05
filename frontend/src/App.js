e2ab1b0b315c374c97d7926c0538ca093319356b
Halderpritam123 <halderpritamhalder1@gmail.com>
2023-10-05 10:14:01 +0530

    basic app model

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Snakeladder from './components/Snakeladder';
+
+const App = () => {
+  return (
+    <div>
+      <Snakeladder/>
+    </div>
+  );
+}
+
+export default App;
